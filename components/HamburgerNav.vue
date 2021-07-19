0b11fb24a972aa2857fa00a4ee3306dae45bd43e
Dan Nguyen <dannguyen8189@gmail.com>
2021-07-19 11:19:51 -0700

    Update HamburgerNav.vue

--- components/HamburgerNav.vue
+++ components/HamburgerNav.vue
@@ -73,50 +73,51 @@ body{
     opacity: 0;
 }
 .menu-wrap .hamburger{
-    position: absolute;
+    position: absolute; /* hamburger menu sits at the top right; position: absolute allows us to do this */
     top: 0;
-    left: calc(100vw - 60px);
+    left: calc(100vw - 60px); /* we don't want the hamburger to hug the right side, but just be close to it*/
     z-index: 1;
     width: 60px;
     height: 60px;
     padding: 1.6rem;
     background: map-get($colors, 'block1');
-    display: flex;
-    align-items: center;
+    display: flex; /* lets us set up the hamburger menu lines down the middle using align-items and justify-content*/
+    align-items: center; 
     justify-content: center;
 }
-/* Hamburger line */
+/* Middle hamburger line */
 .menu-wrap .hamburger > div {
-    position: relative;
+    position: relative; /* lets us position: absolute the other 2 hamburger lines off of this one */
     width: 100%;
     height: 3px;
     background-color: #fff;
     display: flex;
     align-items: center;
     justify-content: center;
-    transition: all 0.4s ease;
+    transition: all 0.4s ease; /* toggle animation speed for the menu */
 }
-/* Top and bottom lines */
+/* duplicate line to form top and bottom lines of the hamburger */
 .menu-wrap .hamburger > div:before,
 .menu-wrap .hamburger > div:after{
     content: '';
     position: absolute;
     z-index: 1;
-    top: -10px;
+    top: -10px; /* move a line up to form the top hamburger line */
     width: 100%;
     height: 3px;
     background: inherit;
 }
-/* Moves line down */
+/* Moves one of the lines down to form the bottom line */
 .menu-wrap .hamburger > div:after{
     top: 10px;
 }
-/* Toggler animate */
+
+/* Animate when user clicks it */
 .menu-wrap .toggler:checked + .hamburger > div{
     transform: rotate(135deg);
     background: map-get($colors, 'accent1');
 }
-/* Turn Lines into X */
+/* Turn hamburger lines into an X shape on click */
 .menu-wrap .toggler:checked + .hamburger > div:before,
 .menu-wrap .toggler:checked + .hamburger > div:after{
     top: 0;
@@ -128,24 +129,26 @@ body{
 }
 /* Show menu */
 .menu-wrap .toggler:checked ~ .menu{
-    visibility: visible;
+    visibility: visible; /* Menu shows up when user clicks the hamburger button */
 }
 .menu-wrap .toggler:checked ~ .menu > div{
-    transform: scale(1);
+    transform: scale(1); /* Expand menu from its initial invisible state on  click */
     transition-duration: .75s;
 }
 .menu-wrap .toggler:checked ~ .menu > div > div{
     opacity: 1;
     transition: opacity 0.4s ease;
 }
+
+/* the menu. It expands from the center from size zero when user clicks the hamburger button and shrinks back to size zero when user clicks 'x' */
 .menu-wrap .menu{
-    position: fixed;
+    position: fixed; /* menu should show up at same spot regardless of where user has scrolled */
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
     font-weight: 700;
-    visibility: hidden; 
+    visibility: hidden; /* menu starts out hidden */
     overflow: hidden;
     display: flex;
     align-items: center;
@@ -153,20 +156,23 @@ body{
 }
 .menu-wrap .menu > div{
     background-color: rgba(24,39,51,0.85);
-    border-radius: 50%;
+    border-radius: 50%; /* menu is enclosed in a circle */
     width: 200vh;
     height: 200vh;
     display: flex;
     flex: none;
     align-items: center;
     justify-content: center;
-    transform: scale(0); 
-    transition: all 0.4s ease;
+    transform: scale(0); /* menu starts out at size zero before growing when user wants to see it */
+    transition: all 0.4s ease; /* the speed the menu grows and shrinks */
     @media screen and (min-width: map-get($breakpoints, large)) {
         width: 200vw;
         height: 200vw;
     }
 }
+
+/* the menu text */
+
 .menu-wrap .menu > div > div{
     text-align: center;
     max-width: 90vw;
